51f91fe0cc4917777d439b24dba665bf23c18d1e
Shicheng Rao <sr307@duke.edu>
2018-07-05 18:08:07 +0300

    minor deletion

--- src/backend/Piece.java
+++ src/backend/Piece.java
@@ -3,8 +3,6 @@ package backend;
 public abstract class Piece {
 	public abstract boolean getColor();
 
-    public abstract boolean isInBoard(int file, int rank) ;
-
     public abstract String algebraicName();
 
     public abstract String fenName();
