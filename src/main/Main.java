1ef4239438ed99edcadced268831b3257a78e0ee
Shicheng Rao <sr307@duke.edu>
2018-07-12 22:25:30 +0300

    bugfix, plus  made main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,19 @@
 package main;
 
-public class Main {
+import frontend.screens.StartScreen;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		StartScreen ss = new StartScreen(primaryStage);
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 	
 }
